1277f4219462afd9b4a801baa5a12511cf63574e
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2019-05-28 22:29:03 +0200

    infinite scroll

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -4,6 +4,15 @@ import { ResponsiveService } from 'app/core/services/responsive.service';
 import { Event } from 'app/shared/models/event';
 import { EventService } from '../events/event.service';
 import { AuthService } from 'app/core/services/auth.service';
+import { isEmpty } from 'app/shared/utility/change-objects';
+
+enum DashBoardState {
+  Error,
+  Empty,
+  Done,
+  Loading,
+  NoMore
+}
 
 @Component({
   selector: 'app-dashboard',
@@ -13,24 +22,52 @@ import { AuthService } from 'app/core/services/auth.service';
 export class DashboardComponent implements OnInit {
   events: Event[] = [];
   bMobile = true;
+  dashBoardState = DashBoardState;
+  currentState = DashBoardState.Loading;
+  private m_offset = 0;
+  private m_sizeToGet = 5;
 
-  constructor(responsiveService: ResponsiveService, private m_eventService: EventService, private m_authService: AuthService) { 
+  constructor(responsiveService: ResponsiveService, private m_eventService: EventService, private m_authService: AuthService) {
     responsiveService.isMobile().subscribe(result => {
       if (result.matches) {
-          this.bMobile = false;
+        this.bMobile = false;
       } else {
-          this.bMobile = true;
+        this.bMobile = true;
       }
     });
+
+    if (!this.bMobile) {
+      this.m_sizeToGet = 15;
+    }
   }
 
   ngOnInit() {
-    this.m_eventService.getEventList(0, 20).subscribe(result => {
+    this.m_eventService.getEventList(this.m_offset, this.m_sizeToGet).subscribe(result => {
+      this.currentState = DashBoardState.Done;
       this.events = result;
     }, error => {
-
+      this.currentState = DashBoardState.Error;
     });
-    
+
+  }
+
+  increaseEventList() {
+    if (this.events.length >= this.m_sizeToGet + this.m_offset) {
+      this.currentState = DashBoardState.Loading;
+      this.m_eventService.getEventList(this.m_offset, this.m_sizeToGet).subscribe(result => {
+        if (isEmpty(result)) {
+          this.currentState = DashBoardState.NoMore;
+        } else {
+          this.events = this.events.concat(result);
+          this.m_offset += this.m_sizeToGet;
+          this.currentState = DashBoardState.Done;
+        }
+      }, error => {
+        this.currentState = DashBoardState.Error;
+      });
+    } else {
+      this.currentState = DashBoardState.NoMore;
+    }
   }
 
 }
